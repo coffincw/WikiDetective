d1119f514b223dd4fdde94a52014cfbc3b1068fa
Zach Schaffer <cursedhydra@gmail.com>
2018-11-03 02:35:37 -0400

    add static folder, js, html, css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 #from util import testing2, math, number
 app = Flask(__name__)
-
+foo = "test"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',foo=foo)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
